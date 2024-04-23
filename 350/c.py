42feb1e5cd83b870ecf9d2123ab74a7aee8f9dea
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-04-23 11:57:09 +0900

    c

--- 350/c.py
+++ 350/c.py
@@ -1,26 +1,21 @@
 n = int(input())
 array = list(map(int, input().split()))
 
-# n = 3
-# array = [3, 1, 2]
-# n = 5
-# array = [3, 4, 1, 2, 5]
-sort_elems = []
-num = 1
+dict = {}
+for idx, elem in enumerate(array):
+    dict[elem] = idx
 
 
-for idx in range(n):
-    if array[idx] > num:
-        sort_elems.append([num, array.index(num) + 1])
-        # 右側のインデックスを変化
-        array[array.index(num)] = array[idx]
-        # 左側のインデックスを変化
-        array[idx] = num
-    num += 1
+sorted_elems = []
+for i in range(n):
+    if array[i] == i + 1:
+        continue
+    j = dict[i + 1]
+    dict[array[i]], dict[array[j]] = dict[array[j]], dict[array[i]]
+    array[i], array[j] = array[j], array[i]
+    sorted_elems.append([i + 1, j + 1])
 
-# print(sort_elems)
-
-print(len(sort_elems))
-if len(sort_elems) != 0:
-    for elem in sort_elems:
+print(len(sorted_elems))
+if len(sorted_elems) != 0:
+    for elem in sorted_elems:
         print(f"{elem[0]} {elem[1]}")
