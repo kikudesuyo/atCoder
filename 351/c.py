78e7d05e8c2f14bdcc0ec4da5c3082357f5148e9
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-04-29 13:14:00 +0900

    3 WA

--- 351/c.py
+++ 351/c.py
@@ -1,18 +1,33 @@
 n = int(input())
 a_s = list(map(int, input().split()))
-idx = 0
-while True:
-    if idx == len(a_s) - 1:
-        print(len(a_s))
-        exit()
-    if len(a_s) == 1:
+a_dict = {}
+for idx in range(n):
+    a_dict[idx] = a_s[idx]
+check_a = []
+input_count = 0
+reverse_flag = False
+while input_count < n:
+    if reverse_flag:
+        if len(check_a) < 2:
+            reverse_flag = False
+            continue
+        if check_a[-1] == check_a[-2]:
+            check_a.pop()
+            check_a[-1] += 1
+            continue
+        else:
+            reverse_flag = False
+            continue
+    if len(check_a) == 0:
+        check_a.append(a_dict[input_count])
+        input_count += 1
         continue
-    if a_s[idx] != a_s[idx + 1]:
-        idx += 1
-    elif a_s[idx] == a_s[idx + 1]:
-        insert_num = a_s.pop(idx) + 1
-        a_s.pop(idx)
-        a_s.insert(idx, insert_num)
-        idx += -1
-        if idx < 0:
-            idx = 0
+    if check_a[-1] == a_dict[input_count]:
+        check_a[-1] += 1
+        # input_count += 1
+        reverse_flag = True
+        continue
+    elif check_a[-1] != a_dict[input_count]:
+        check_a.append(a_dict[input_count])
+        input_count += 1
+print(len(check_a))
