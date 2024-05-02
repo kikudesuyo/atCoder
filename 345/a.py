c914fe7257fb0b7dd498eef9146a693d149b455b
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-05-02 22:00:23 +0900

    a

---
+++ 345/a.py
@@ -0,0 +1,14 @@
+s = input()
+
+if s[0] != "<":
+    print("No")
+    exit()
+if s[-1] != ">":
+    print("No")
+    exit()
+for i in range(1, len(s) - 1):
+    if s[i] != "=":
+        print("No")
+        exit()
+
+print("Yes")
