e347656efab466d7a9c4054911177b838eae5fe5
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-05-05 11:16:36 +0900

    not resolved timeout

---
+++ 352/d.py
@@ -0,0 +1,19 @@
+n, k = list(map(int, input().split()))
+p_s = list(map(int, input().split()))
+
+dict = {}
+for i in range(len(p_s)):
+    dict[p_s[i]] = i + 1
+diffs = []
+good_array = []
+for i in range(1, len(p_s) + 2 - k):
+    if i == 1:
+        for j in range(1, k + 1):
+            good_array.append(dict[j])
+    else:
+        good_array.pop(0)
+        good_array.append(dict[i + k - 1])
+    diff = max(good_array) - min(good_array)
+    diffs.append(diff)
+
+print(min(diffs))
