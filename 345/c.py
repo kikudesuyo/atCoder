c8f7c3239d4dccfe6083b8e0316ef7d1333c43eb
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-05-10 18:12:07 +0900

    d

---
+++ 345/c.py
@@ -0,0 +1,30 @@
+s = list(input())
+
+# alphabets = "abcdefghijklmnopqrstuvwxyz"
+# dict = {}
+# for i in alphabets:
+#     dict[i] = s.count(i)
+
+count = 0
+for i in range(0, len(s) - 1):
+    s[i:]
+
+
+if count == 0:
+    print(1)
+else:
+    print(count)
+
+
+# count = 0
+# for i in range(len(s) - 1):
+#     for j in range(i, len(s)):
+#         # print(s[i], s[j])
+#         if s[i] == s[j]:
+#             continue
+#         else:
+#             count += 1
+# if count == 0:
+#     print(1)
+# else:
+#     print(count)
--- 352/d.py
+++ 352/d.py
@@ -1,19 +1,37 @@
-n, k = list(map(int, input().split()))
-p_s = list(map(int, input().split()))
+# n, k = list(map(int, input().split()))
+# p_s = list(map(int, input().split()))
 
-dict = {}
-for i in range(len(p_s)):
-    dict[p_s[i]] = i + 1
-diffs = []
-good_array = []
-for i in range(1, len(p_s) + 2 - k):
-    if i == 1:
-        for j in range(1, k + 1):
-            good_array.append(dict[j])
-    else:
-        good_array.pop(0)
-        good_array.append(dict[i + k - 1])
-    diff = max(good_array) - min(good_array)
-    diffs.append(diff)
+# dict = {}
+# for i in range(len(p_s)):
+#     dict[p_s[i]] = i + 1
+# diffs = []
+# good_array = []
+# for i in range(1, len(p_s) + 2 - k):
+#     if i == 1:
+#         for j in range(1, k + 1):
+#             good_array.append(dict[j])
+#     else:
+#         good_array.pop(0)
+#         good_array.append(dict[i + k - 1])
+#     diff = max(good_array) - min(good_array)
+#     diffs.append(diff)
+# print(min(diffs))
 
-print(min(diffs))
+###アルゴリズムは同じ、SortedSetを使用することでオーダーがlogNとなりACになる
+N, K = map(int, input().split())
+P = list(map(int, input().split()))
+R = [None for _ in range(N)]
+for n in range(N):
+    R[P[n] - 1] = n + 1
+from sortedcontainers import SortedSet
+
+SS = SortedSet()
+for n in range(K):
+    SS.add(R[n])
+min = SS[K - 1] - SS[0]
+for n in range(1, N - K + 1):
+    SS.discard(R[n - 1])
+    SS.add(R[n + K - 1])
+    if min is None or SS[K - 1] - SS[0] < min:
+        min = SS[K - 1] - SS[0]
+print(min)
