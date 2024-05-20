3bdd331f11e62a304d349f946daf6185ebca7947
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-05-20 10:57:27 +0900

    c

--- Beginner/354/c.py
+++ Beginner/354/c.py
@@ -1,36 +1,26 @@
 n = int(input())
-
-a_cards = []
-a_idx_dict = {}
-n_list = []
-a_key_dict = {}
-c_key_dict = {}
+a = []
+a_to_c = {}
+a_to_idx = {}
 for i in range(n):
-    a, s = list(map(int, input().split()))
-    a_cards.append(a)
-    a_key_dict[a] = s
-    c_key_dict[s] = a
-    a_idx_dict[a] = i
-    n_list.append(i + 1)
-a_sort = sorted(a_cards)
-a_key_sorted_dict = {key: a_key_dict[key] for key in a_sort}
-removed_arr = []
-for sorted_idx, a_i in enumerate(a_sort):
-    now_c = a_key_sorted_dict[a_i]
-    next_c = a_key_sorted_dict[a_sort[sorted_idx + 1]]
-    if now_c > next_c:
-        removed_a_i = c_key_dict[now_c]
-        removed_idx = a_idx_dict[removed_a_i]
-        removed_arr.append(removed_idx + 1)
-    if sorted_idx == n - 2:
+    a_i, c_i = list(map(int, input().split()))
+    a.append(a_i)
+    a_to_c[a_i] = c_i
+    a_to_idx[a_i] = i + 1
+
+asc_a = sorted(a)
+
+removed_idxes = []
+for i in range(n):
+    a_i, next_a_i = asc_a[i], asc_a[i + 1]
+    if a_to_c[a_i] > a_to_c[next_a_i]:
+        removed_idxes.append(a_to_idx[a_i])
+    if i == n - 2:
         break
 
-print(n - len(removed_arr))
-for i in n_list:
-    if i not in removed_arr:
+
+print(n - len(removed_idxes))
+for i in range(1, n + 1):
+    if i not in removed_idxes:
         print(i, end=" ")
 print()
-# result_list = [str(item) for item in n_list if item not in removed_arr]
-
-# print(len(result_list))
-# print(" ".join(result_list))
