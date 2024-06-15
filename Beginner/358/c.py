6116ca40403b9b62579870faf4d97e9e34f3d653
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-16 00:41:57 +0900

    bit calculation

--- Beginner/358/c.py
+++ Beginner/358/c.py
@@ -1,86 +1,19 @@
-# import itertools
-
-# n, m = map(int, input().split())
-# s_s = []
-# for i in range(n):
-#     s_s.append(input())
-
-# result = m
-# popcorns = ["x"] * m
-# store_count = 0
-# count_flag = False
-# round_robin_s_s = list((itertools.permutations(s_s)))
-# round_robin_s_s = [list(s) for s in round_robin_s_s]
-
-
-# for s in range(len(round_robin_s_s)):
-#     for i in range(n):
-#         for j in range(m):
-#             if round_robin_s_s[s][i][j] == "o" and popcorns[j] == "x":
-#                 popcorns[j] = "o"
-#                 count_flag = True
-#         if count_flag:
-#             store_count += 1
-#             count_flag = False
-#         if popcorns.count("o") == m:
-#             if store_count < result:
-#                 result = store_count
-#     store_count = 0
-#     popcorns = ["x"] * m
-
-# print(result)
-
-
 n, m = map(int, input().split())
-s_s = []
-for i in range(n):
-    s_s.append(input())
+s_s = [input() for _ in range(n)]
 
-strip_idxes = []
-stripped_s_s = s_s.copy()
-for r in range(len(s_s)):
-    my_store = s_s[r]
-    for i in range(len(s_s)):
-        if i == r:
-            continue
-        oppnent_store = s_s[i]
-        number_of_opponent = 0
-        for j in range(m):
-            if my_store[j] == "o" and oppnent_store[j] == "x":
-                number_of_opponent = 0
-                break
-            if oppnent_store[j] == "o":
-                number_of_opponent += 1
-        if number_of_opponent > my_store.count("o"):
-            strip_idxes.append(r)
-            break
-
-for i in sorted(strip_idxes, reverse=True):
-    stripped_s_s.pop(i)
-
-
-import itertools
 
 result = m
-popcorns = ["x"] * m
-store_count = 0
-count_flag = False
-round_robin_s_s = list((itertools.permutations(stripped_s_s)))
-round_robin_s_s = [list(s) for s in round_robin_s_s]
+for bit in range(1 << n):
+    current_stats = ["x"] * m
+    str_boolean = bin(bit)[2:].zfill(n)
+    for idx, boolean in enumerate(str_boolean):
+        if boolean == "1":
+            selected_shop = s_s[idx]
+            for j in range(len(selected_shop)):
+                if selected_shop[j] == "o" and current_stats[j] == "x":
+                    current_stats[j] = "o"
+            if current_stats.count("o") == m:
+                if result > str_boolean.count("1"):
+                    result = str_boolean.count("1")
 
-
-for s in range(len(round_robin_s_s)):
-    for i in range(n):
-        for j in range(m):
-            if round_robin_s_s[s][i][j] == "o" and popcorns[j] == "x":
-                popcorns[j] = "o"
-                count_flag = True
-        if count_flag:
-            store_count += 1
-            count_flag = False
-        if popcorns.count("o") == m:
-            if store_count < result:
-                result = store_count
-    store_count = 0
-    popcorns = ["x"] * m
 print(result)
