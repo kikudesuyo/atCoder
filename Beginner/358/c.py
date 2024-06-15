379667ba15be3d1bcfa4d8c7b0d1b83d3ba435da
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-16 01:09:45 +0900

    modify

--- Beginner/358/c.py
+++ Beginner/358/c.py
@@ -1,19 +1,19 @@
 n, m = map(int, input().split())
 s_s = [input() for _ in range(n)]
 
-
 result = m
 for bit in range(1 << n):
-    current_stats = ["x"] * m
+    current_status = ["x"] * m
     str_boolean = bin(bit)[2:].zfill(n)
     for idx, boolean in enumerate(str_boolean):
-        if boolean == "1":
-            selected_shop = s_s[idx]
-            for j in range(len(selected_shop)):
-                if selected_shop[j] == "o" and current_stats[j] == "x":
-                    current_stats[j] = "o"
-            if current_stats.count("o") == m:
-                if result > str_boolean.count("1"):
-                    result = str_boolean.count("1")
+        if boolean == "0":
+            continue
+        selected_shop = s_s[idx]
+        for j in range(len(selected_shop)):
+            if selected_shop[j] == "o":
+                current_status[j] = "o"
+        if current_status.count("o") == m:
+            if result > str_boolean.count("1"):
+                result = str_boolean.count("1")
 
 print(result)
