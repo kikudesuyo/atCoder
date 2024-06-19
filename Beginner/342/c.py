056f46b332b1d67894a61c4cd0346fd12b692ae2
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-19 22:31:17 +0900

    c

---
+++ Beginner/342/c.py
@@ -0,0 +1,10 @@
+import string
+
+n, s, q = int(input()), input(), int(input())
+alphatbet_str = string.ascii_lowercase
+changed_str = string.ascii_lowercase
+for i in range(q):
+    c, d = input().split()
+    changed_str = changed_str.replace(c, d)
+
+print(s.translate(str.maketrans(alphatbet_str, changed_str)))
