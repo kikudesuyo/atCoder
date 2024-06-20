c10d5508382e58e2636c83adc77a199089a0348c
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-20 16:29:15 +0900

    b

---
+++ Beginner/340/b.py
@@ -0,0 +1,14 @@
+q = int(input())
+
+queries = []
+
+for _ in range(q):
+    type, command = map(int, input().split())
+    queries.append([type, command])
+a_s = []
+for type, command in queries:
+    if type == 1:
+        a_s.append(command)
+    else:
+        idx = len(a_s) - command
+        print(a_s[idx])
