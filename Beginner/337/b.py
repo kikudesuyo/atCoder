98bd6c4c861f7a81e764af91c3fbd91b50d873d5
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-25 19:29:40 +0900

    b

--- Beginner/337/b.py
+++ Beginner/337/b.py
@@ -1,18 +1,11 @@
 s = input()
+a_count = s.count("A")
+b_count = s.count("B")
+c_count = s.count("C")
 
-for i in range(len(s)):
-    if i == len(s) - 1:
-        break
-    if s[i] == "B" and s[i + 1] == "A":
-        print("No")
-        exit()
-    elif s[i] == "C" and (s[i + 1] == "B" or s[i + 1] == "A"):
-        print("No")
-        exit()
-    elif s[i] == "A" and s[i + 1] == "C":
-        for j in range(i + 2, len(s)):
-            if s[j] == "B":
-                print("No")
-                exit()
+ideal_string = "A" * a_count + "B" * b_count + "C" * c_count
 
-print("Yes")
+if s == ideal_string:
+    print("Yes")
+else:
+    print("No")
