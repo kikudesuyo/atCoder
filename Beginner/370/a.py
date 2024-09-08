5b0a3fa8250030dcd2c4b0661a3f02b4ae0dbdb5
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-09-08 13:34:12 +0900

    a

---
+++ Beginner/370/a.py
@@ -0,0 +1,10 @@
+l, r = map(int, input().split())
+
+if l == 0 and r == 0:
+    print("Invalid")
+elif l == 1 and r == 1:
+    print("Invalid")
+elif l == 1:
+    print("Yes")
+else:
+    print("No")
