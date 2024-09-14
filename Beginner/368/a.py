6181eea60ebdc279a1ce6189a36c44eaec8ae99a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-09-14 18:43:40 +0900

    a

--- Beginner/368/a.py
+++ Beginner/368/a.py
@@ -1,4 +1,6 @@
 n, k = map(int, input().split())
 a_s = list(map(int, input().split()))
 
-print("YES" if n - k >= k - 1 else "NO")
+answers = a_s[(-1) * k :] + a_s[: (-1) * k]
+
+print(" ".join(map(str, answers)))
