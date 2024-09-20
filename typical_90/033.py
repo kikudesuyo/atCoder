157bd0e7b6425b9a98525e0035d24e52bc06369f
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-09-21 00:52:48 +0900

    modify33

--- typical_90/033.py
+++ typical_90/033.py
@@ -1,14 +1,12 @@
 h, w = map(int, input().split())
 
 count = 0
-if h == 1:
-    count = w
-elif w == 1:
-    count = h
+if h == 1 or w == 1:
+    # 条件を満たしていないので全て点灯させてOK
+    count = h * w
 else:
     for i in range(h):
         for j in range(w):
             if i % 2 == 0 and j % 2 == 0:
                 count += 1
-
 print(count)
