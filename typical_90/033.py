52531ec2f7859e77380defa9f069e10e2426fdf5
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-09-21 00:51:19 +0900

    modify33t

--- typical_90/033.py
+++ typical_90/033.py
@@ -1,33 +1,14 @@
 h, w = map(int, input().split())
 
-
-if h == 1 or w == 1:
-    print(h * w)
-    exit()
-led_matrix = [[False for _ in range(w)] for _ in range(h)]
-initial_on_off = True
-for i in range(h):
-    for j in range(w):
-        if i == 0:
-            led_matrix[i][j] = initial_on_off
-            initial_on_off = not initial_on_off
-            continue
-        if j == 0:
-            if not led_matrix[i - 1][j]:
-                led_matrix[i][j] = True
-            continue
-        contditions = [
-            led_matrix[i - 1][j],
-            led_matrix[i][j - 1],
-            led_matrix[i - 1][j - 1],
-        ]
-        if not any(contditions):
-            led_matrix[i][j] = True
-
-
 count = 0
-for i in range(h):
-    for j in range(w):
-        if led_matrix[i][j]:
-            count += 1
+if h == 1:
+    count = w
+elif w == 1:
+    count = h
+else:
+    for i in range(h):
+        for j in range(w):
+            if i % 2 == 0 and j % 2 == 0:
+                count += 1
+
 print(count)
