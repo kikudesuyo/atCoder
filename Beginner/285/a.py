ea73aaeb95296f5334ef2779f08bcdcdcf809e0a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-09-25 22:55:58 +0900

    solved

---
+++ Beginner/285/a.py
@@ -0,0 +1,65 @@
+a, b = map(int, input().split())
+
+
+if a == 1:
+    if b in [2, 3]:
+        print("Yes")
+        exit()
+elif a == 2:
+    if b in [4, 5]:
+        print("Yes")
+        exit()
+elif a == 3:
+    if b in [6, 7]:
+        print("Yes")
+        exit()
+elif a == 4:
+    if b in [8, 9]:
+        print("Yes")
+        exit()
+elif a == 5:
+    if b in [10, 11]:
+        print("Yes")
+        exit()
+elif a == 6:
+    if b in [12, 13]:
+        print("Yes")
+        exit()
+elif a == 7:
+    if b in [14, 15]:
+        print("Yes")
+        exit()
+elif a == 8:
+    if b in [4]:
+        print("Yes")
+        exit()
+elif a == 9:
+    if b in [4]:
+        print("Yes")
+        exit()
+elif a == 10:
+    if b in [5]:
+        print("Yes")
+        exit()
+elif a == 11:
+    if b in [5]:
+        print("Yes")
+        exit()
+elif a == 12:
+    if b in [6]:
+        print("Yes")
+        exit()
+elif a == 13:
+    if b in [6]:
+        print("Yes")
+        exit()
+elif a == 14:
+    if b in [7]:
+        print("Yes")
+        exit()
+elif a == 15:
+    if b in [7]:
+        print("Yes")
+        exit()
+
+print("No")
