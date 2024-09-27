1c525eea5d4125305e1e05ae2431e1d4757c2737
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-09-28 01:48:23 +0900

    solved

--- Beginner/259/a.py
+++ Beginner/259/a.py
@@ -1,9 +1,16 @@
 n, m, x, t, d = map(int, input().split())
 
 
-tall = t
-for i in range(m):
-    if i == x:
-        break
-    tall += d
-print(tall)
+baby_tall = t - (d * x)
+
+for i in range(n + 1):
+    if i == 0:
+        talls = [baby_tall]
+        temp = baby_tall
+        continue
+    if i <= x:
+        temp += d
+    talls.append(temp)
+
+
+print(talls[m])
