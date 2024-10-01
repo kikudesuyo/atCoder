5628373b5b7b7e0544fe0a1b0f3dcf766d9fd84a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-01 11:25:17 +0900

    solved

---
+++ Beginner/254/b.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+
+def pascal(n):
+    pascal = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        row.append(1)
+        pascal.append(row)
+    return pascal
+
+
+for row in pascal(n):
+    print(" ".join(map(str, row)))
