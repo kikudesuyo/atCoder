0b3efc8f4d756225d38c909e48ced21e92ddbeb5
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-04 14:56:56 +0900

    solved

--- Beginner/257/b.py
+++ Beginner/257/b.py
@@ -2,6 +2,7 @@ n, k, q = map(int, input().split())
 a_k = list(map(int, input().split()))
 l_q = list(map(int, input().split()))
 
+
 for i in range(q):
     if l_q[i] == k:
         if a_k[l_q[i] - 1] == n:
