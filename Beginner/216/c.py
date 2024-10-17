3aa60055b00da36119041dce2d75ca61c205298e
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-17 16:23:21 +0900

    solved

---
+++ Beginner/216/c.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+
+ans = []
+while n > 0:
+    if n % 2 == 1:
+        ans.append("A")
+        n -= 1
+    else:
+        ans.append("B")
+        n //= 2
+
+print("".join(ans[::-1]))
