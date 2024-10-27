480c4ce9093a2023e539cdc6653c7f31c6c08359
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-28 00:57:45 +0900

    solved

--- Beginner/273/b.py
+++ Beginner/273/b.py
@@ -1,3 +1,12 @@
+from decimal import Decimal
+
 x, k = map(int, input().split())
 
-answer = str(x)
+
+def f(x, k):
+    return Decimal(x).quantize(Decimal(f"1E{k}"), rounding="ROUND_HALF_UP")
+
+
+for i in range(k):
+    x = f(x, i + 1)
+print(int(x))
