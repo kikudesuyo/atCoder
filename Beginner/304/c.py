7d286aca35e847d46aaca90f6d767efb5bd6abc6
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-11-02 14:35:02 +0900

    solved

--- Beginner/304/c.py
+++ Beginner/304/c.py
@@ -2,18 +2,18 @@ n, d = map(int, input().split())
 xy_n = [tuple(map(int, input().split())) for _ in range(n)]
 
 
+is_infected = [False] * n
+is_infected[0] = True
+
 set_xy = set()
 set_xy.add(xy_n[0])
 
-is_infected = [False] * n
-
-
 while set_xy:
-    xy = set_xy.pop()
+    x, y = set_xy.pop()
     for i in range(n):
         if is_infected[i]:
             continue
-        if (xy[0] - xy_n[i][0]) ** 2 + (xy[1] - xy_n[i][1]) ** 2 <= d**2:
+        if (x - xy_n[i][0]) ** 2 + (y - xy_n[i][1]) ** 2 <= d**2:
             set_xy.add(xy_n[i])
             is_infected[i] = True
 
