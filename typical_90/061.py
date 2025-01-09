d693f0243fbefcf20dbf08625c780bffd579c3c0
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-01-09 14:25:36 +0900

    solved

--- typical_90/061.py
+++ typical_90/061.py
@@ -2,12 +2,15 @@ q = int(input())
 tx_s = [list(map(int, input().split())) for _ in range(q)]
 
 
-first_half, second_half = [], []
+first_half = []
+second_half = []
 for t, x in tx_s:
     if t == 1:
         first_half.append(x)
     elif t == 2:
         second_half.append(x)
     else:
-        deck = first_half[::-1] + second_half
-        print(deck[x - 1])
+        if len(first_half) >= x:
+            print(first_half[-x])
+        else:
+            print(second_half[x - len(first_half) - 1])
