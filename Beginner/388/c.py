bbc546459e978b581dd9c87e2c0c0117d3fa5b16
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-01-11 23:49:36 +0900

    modify name

--- Beginner/388/c.py
+++ Beginner/388/c.py
@@ -1,14 +1,14 @@
 n = int(input())
 a_n = list(map(int, input().split()))
 
-base_mothi_idx = 0
+base_mochi_idx = 0
 
 cnt = 0
 for top_idx in range(n):
-    top_mothi = a_n[top_idx]
-    while top_mothi * 2 > a_n[base_mothi_idx] and base_mothi_idx < n - 1:
-        base_mothi_idx += 1
-    if top_mothi * 2 <= a_n[base_mothi_idx]:
-        cnt += n - base_mothi_idx
+    top_mochi = a_n[top_idx]
+    while top_mochi * 2 > a_n[base_mochi_idx] and base_mochi_idx < n - 1:
+        base_mochi_idx += 1
+    if top_mochi * 2 <= a_n[base_mochi_idx]:
+        cnt += n - base_mochi_idx
 
 print(cnt)
--- Beginner/388/e.py
+++ Beginner/388/e.py
@@ -2,18 +2,18 @@ n = int(input())
 a_n = list(map(int, input().split()))
 
 if n % 2 == 0:
-    top_mothies = a_n[: n // 2]
-    base_mothies = a_n[n // 2 :]
+    top_mochies = a_n[: n // 2]
+    base_mochies = a_n[n // 2 :]
 else:
-    top_mothies = a_n[: n // 2]
-    base_mothies = a_n[n // 2 + 1 :]
+    top_mochies = a_n[: n // 2]
+    base_mochies = a_n[n // 2 + 1 :]
 
 
 top_idx = 0
 base_idx = 0
 cnt = 0
-while top_idx < len(top_mothies) and base_idx < len(base_mothies):
-    if top_mothies[top_idx] * 2 <= base_mothies[base_idx]:
+while top_idx < len(top_mochies) and base_idx < len(base_mochies):
+    if top_mochies[top_idx] * 2 <= base_mochies[base_idx]:
         cnt += 1
         top_idx += 1
         base_idx += 1
