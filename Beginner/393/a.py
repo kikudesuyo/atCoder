d090320a4220b55514e566a8b68bbed831ced0f7
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-02-22 20:57:03 +0900

    solved

---
+++ Beginner/393/a.py
@@ -0,0 +1,11 @@
+t, a = input().split()
+if t == "fine":
+    if a == "fine":
+        print("4")
+    else:
+        print("3")
+else:
+    if a == "fine":
+        print("2")
+    else:
+        print("1")
