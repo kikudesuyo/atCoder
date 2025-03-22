03db6092a5c14d6d99060d5d58a1eac45688a46b
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-03-22 23:59:42 +0900

    resolve

--- Beginner/398/d.py
+++ Beginner/398/d.py
@@ -2,30 +2,25 @@ n, r, c = map(int, input().split())
 s = list(input())
 
 smokes = set()
-
 smokes.add((0, 0))
-
-target = (r, c)
-current = (0, 0)
+takahashi = (r, c)
+smoke_source = (0, 0)
 ans = []
 for i in range(n):
     if s[i] == "N":
-        target = (target[0] + 1, target[1])
-        current = (current[0] + 1, current[1])
-        smokes.add(current)
+        takahashi = (takahashi[0] + 1, takahashi[1])
+        smoke_source = (smoke_source[0] + 1, smoke_source[1])
     elif s[i] == "W":
-        target = (target[0], target[1] + 1)
-        current = (current[0], current[1] + 1)
-        smokes.add(current)
+        takahashi = (takahashi[0], takahashi[1] + 1)
+        smoke_source = (smoke_source[0], smoke_source[1] + 1)
     elif s[i] == "S":
-        target = (target[0] - 1, target[1])
-        current = (current[0] - 1, current[1])
-        smokes.add(current)
+        takahashi = (takahashi[0] - 1, takahashi[1])
+        smoke_source = (smoke_source[0] - 1, smoke_source[1])
     elif s[i] == "E":
-        target = (target[0], target[1] - 1)
-        current = (current[0], current[1] - 1)
-        smokes.add(current)
-    if target in smokes:
+        takahashi = (takahashi[0], takahashi[1] - 1)
+        smoke_source = (smoke_source[0], smoke_source[1] - 1)
+    smokes.add(smoke_source)
+    if takahashi in smokes:
         ans.append(1)
     else:
         ans.append(0)
