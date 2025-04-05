bda01de1f0787996a162ff44165f4b784a336110
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-04-06 01:17:30 +0900

    refacor: resolve

---
+++ Beginner/391/a.py
@@ -0,0 +1,18 @@
+d = input()
+
+if d == "N":
+    print("S")
+if d == "S":
+    print("N")
+if d == "E":
+    print("W")
+if d == "W":
+    print("E")
+if d == "NE":
+    print("SW")
+if d == "NW":
+    print("SE")
+if d == "SE":
+    print("NW")
+if d == "SW":
+    print("NE")
