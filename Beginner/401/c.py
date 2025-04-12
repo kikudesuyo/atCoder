eb0fb94d9a3f218e64ee460435e9d21364141ded
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-04-12 23:11:35 +0900

    resolve

---
+++ Beginner/401/c.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+if n < k:
+    print(1)
+    exit()
+
+d = deque()
+for i in range(k):
+    d.append(1)
+
+cur_sum_num = k
+for i in range(n - k):
+    cur_sum_num %= 10**9
+    d.append(cur_sum_num)
+    cur_sum_num += cur_sum_num - d.popleft()
+
+
+print(cur_sum_num % 10**9)
